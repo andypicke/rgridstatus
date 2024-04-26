f28fe03cb75861a8b3aa98354633d4a7d4f034f8
Andy <andypicke@gmail.com>
2024-04-26 10:06:41 -0600

    add get_dataset_updates function

---
+++ R/get_dataset_updates.R
@@ -0,0 +1,24 @@
+#' Get list of dataset updates that have occurred
+#' @param wh_dataset Name of dataset to download (default is all)
+#' @param limit Max number of updates to fetch. default 100
+#' @param api_key API key. Default is "GRIDSTATUS_API_KEY" stored in .Renviron file
+
+get_dataset_updates <- function(wh_dataset = "all", limit = 100, api_key = Sys.getenv("GRIDSTATUS_API_KEY")){
+
+  # base url for the Gridstatusio API
+  base_url <- "https://api.gridstatus.io/v1/"
+
+  if (wh_dataset != "all") {
+    req_url <- paste0(base_url,"dataset-updates/", wh_dataset, "?limit=", limit)
+  } else {
+    req_url <- paste0(base_url,"dataset-updates?limit=", limit)
+  }
+
+  resp <- httr::GET(req_url, httr::add_headers('x-api-key' = api_key))
+
+  resp_parsed <- jsonlite::fromJSON(httr::content(resp, as = 'text'))
+
+  updates <- resp_parsed$data
+
+  return(updates)
+}
